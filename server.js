d26abcc929e46278c634801cc7b3072b1d160a39
dwdarm <fajardwidarm@gmail.com>
2020-03-29 12:43:18 +0700

    delete rate limiter

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const rateLimit = require("express-rate-limit");
 const helmet = require('helmet');
 const cors = require('cors');
 const server = express();
@@ -8,10 +7,6 @@ const db = require('./db');
 
 server.set('trust proxy', 1);
 server.use(express.static(__dirname + '/public'));
-server.use(rateLimit({
-  windowMs: 5 * 60 * 1000, // 5 minutes
-  max: 100
-}));
 server.use(cors());
 server.use(helmet());
 
